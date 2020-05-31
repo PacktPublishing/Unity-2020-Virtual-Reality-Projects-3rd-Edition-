6b81b210bbf6d3d70a67a35cd8d6c1b74aba9442
Jonathan Linowes <linojon@gmail.com>
2020-05-31 14:55:24 -0400

    Chapter09

---
+++ UVRP3Projects/Assets/_UVRP3Assets/Chapter09/Scripts/BallGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGame : MonoBehaviour
+{
+    public Transform dropPoint;
+    public float interval = 3f;
+
+    private float nextBallTime = 0f;
+    private ObjectPooler objectPooler;
+    private AudioSource soundEffect;
+
+    void Start()
+    {
+        objectPooler = GetComponent<ObjectPooler>();
+        soundEffect = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Time.time > nextBallTime)
+        {
+            NewBall(dropPoint.position);
+            nextBallTime = Time.time + interval;
+        }
+    }
+
+    private void NewBall(Vector3 position)
+    {
+        GameObject ball = objectPooler.GetPooledObject();
+        if (ball != null)
+        {
+            ball.transform.position = position;
+            ball.transform.rotation = Quaternion.identity;
+
+            Rigidbody rb = ball.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+            ball.SetActive(true);
+            soundEffect.Play();
+        }
+    }
+}
---
+++ UVRP3Projects/Assets/_UVRP3Assets/Chapter09/Scripts/BallsFromHeaven.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallsFromHeaven : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public float startHeight = 10f;
+    public float interval = 0.5f;
+
+    private float nextBallTime = 0f;
+
+    private void Start()
+    {
+        nextBallTime = Time.time + interval;
+    }
+
+    void Update()
+    {
+        if (Time.time > nextBallTime)
+        {
+            Vector3 position = new Vector3(
+                Random.Range(-4f, 4f),
+                startHeight,
+                Random.Range(-4f, 4f));
+            NewBall(position);
+
+            nextBallTime = Time.time + interval;
+        }
+    }
+
+    private void NewBall(Vector3 position)
+    {
+        Instantiate(ballPrefab, position, Quaternion.identity);
+    }
+}
