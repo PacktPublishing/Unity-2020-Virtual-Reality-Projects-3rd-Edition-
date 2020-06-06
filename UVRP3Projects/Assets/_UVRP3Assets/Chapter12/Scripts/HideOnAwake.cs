ca5811fa8bddc1c505030e410ba87954301d588f
Jonathan Linowes <linojon@gmail.com>
2020-06-06 14:30:47 -0400

    Chapter12

---
+++ UVRP3Projects/Assets/_UVRP3Assets/Chapter12/Scripts/HideOnAwake.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOnAwake : MonoBehaviour
+{
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ UVRP3Projects/Assets/_UVRP3Assets/Chapter12/Scripts/SkyboxExposure.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxExposure : MonoBehaviour
+{
+    public Material skyboxMaterial;
+    public float exp = 1.0f;
+
+    private float previousExp = 0f;
+
+    private void Update()
+    {
+        if (exp != previousExp)
+        {
+            skyboxMaterial.SetFloat("_Exposure", exp);
+            previousExp = exp;
+        }
+    }
+}
